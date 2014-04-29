31c251085f97403f5f2a3dd256ede1b1e0200ba6
Sebastian Wilzbach <sebi@wilzbach.me>
2014-04-29 19:27:53 +0200

    initial commit
    Former-commit-id: 0e972dc97387e849b9cc2a636195302f35ce0daa

---
+++ js/simple-depend.js
@@ -0,0 +1,14 @@
+//my/shirt.js now has some dependencies, a cart and inventory
+//module in the same directory as shirt.js
+define(["./cart", "./inventory"], function(cart, inventory) {
+        //return an object to define the "my/shirt" module.
+        return {
+            color: "blue",
+            size: "large",
+            addToCart: function() {
+                inventory.decrement(this);
+                cart.add(this);
+            }
+        }
+    }
+);
---
+++ js/util.coffee
@@ -0,0 +1,5 @@
+define [], () ->
+  toDom: (text) ->
+    # This is a contrived example, this approach is not realistic,
+    # just used as a demonstration.
+    return "hi"
