c64db9fe3acf2e5b6722c3c8fef3403f03c10b86
Sebastian Wilzbach <sebi@wilzbach.me>
2014-05-24 16:54:47 +0200

    snippets as examples included
    Former-commit-id: c00cee994d3021c07fd2f22ce72ac9b5925bb1a4 [formerly 6dcac58917e683bb68bf37926cdd42f7ef7891ca]
    Former-commit-id: 2b306fd5120f628ca0641a2533e958677eb563b7

---
+++ doc-js/simple_io.js
@@ -0,0 +1,14 @@
+require(["jquery","cs!seq", "cs!input/fasta"], function ($, Seq, Fasta) {
+
+  a_sequence = new Seq("ACGT");
+  $("#ele").append("rvs_complement  " + a_sequence.reverse_complement() + "\n");
+  $("#ele").append("Transcribe:     " + a_sequence.transcribe()+ "\n");
+  $("#ele").append("Back_Transcribe " + a_sequence.back_transcribe()+ "\n");
+  $("#ele").append("Translate       " + a_sequence.translate()+ "\n");
+  $("#ele").append("Ungap           " + a_sequence.ungap()+ "\n");
+
+  // uniprot does not support SSL requests
+  dummyFasta = new Fasta("dummy/S5G603.fasta");
+  $("#ele").append("\n Dummy FASTA: \n" + dummyFasta.toString()+ "\n");
+
+});
