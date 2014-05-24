6dcac58917e683bb68bf37926cdd42f7ef7891ca
Sebastian Wilzbach <sebi@wilzbach.me>
2014-05-24 16:54:47 +0200

    snippets as examples included

---
+++ doc-js/simple_msa.js
@@ -0,0 +1,14 @@
+require(["msa/msa", "msa/sequence"], function (MSA, Sequence) {
+
+  msa = new MSA('msa-first-example');
+  msa.setConsole('msa-first-console');
+  msa.addSequences([new Sequence("MSPFTACAPDRLNNPPLKRTRTLSLRNPSETSTLSRSDRRNSMFLEGERIMNAFGQQPSS", "awesome name", 1)
+    ,new Sequence("QQTSPLQQQDILDMTVYCDSNFSMYQQNLHHHHHHHHHQRPPAHPSGYGLGEYSSPSTNP", "awesome name3", 2)
+    ,new Sequence("YLWMNSPGITSTPYLSSPNGGSYIQSGFGSNQRQFLPPPTGFGSADLGWLSISSQQELFK", "awesome name2", 3)
+    ,new Sequence("MVRPPYSYSALIAMAIQNAQDKKLTLSQIYQYVADNFPFYKKSKAGWQNSIRHNLSLNDC", "awesome name5", 4)
+    ,new Sequence("FKKVARDEDDPGKGNYWTLDPNCEKMFDNGNFRRKRKRRADGNAMSVKSEDALKLADTSS", "awesome name4", 5)
+    ,new Sequence("LMSASPPSLQNSPTSSDPKSSPSPSAEHSPCFSNFIGNMNSIMSGNAVRSRDGSSAHLGD", "awesome name7", 6)
+    ,new Sequence("FTQHGMSGHEISPPSEPGHLNTNRLNYYSASHNNSGLINSISNHFSVNNLIYNRDGSEV", "awesome name6", 7)]);
+  //	msa.menu.displayMenuElement();
+});
+
