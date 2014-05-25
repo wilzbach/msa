80a52ce42c29189035d349711f5ef3d2c5be0945
Sebastian Wilzbach <sebi@wilzbach.me>
2014-05-25 07:59:46 +0200

    added snippets for milestone 1

---
+++ doc-js/msa_auto-fit.js
@@ -0,0 +1,6 @@
+require(["msa/msa"], function (MSA) {
+
+  var msa = new MSA('msa-auto-fit');
+
+  msa.addDummySequences();
+});
---
+++ doc-js/msa_highlighting.js
@@ -0,0 +1,7 @@
+require(["msa/msa"], function (MSA) {
+
+  var msa = new MSA('msa-highlighting');
+  msa.setConsole('msa-highlighting-console');
+
+  msa.addDummySequences();
+});
