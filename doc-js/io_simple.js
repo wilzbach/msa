ab12a040dd272d75db184b988f39279f9a398d90
Sebastian Wilzbach <sebi@wilzbach.me>
2014-05-25 12:42:10 +0200

    awesome generic file reading integrated
    Former-commit-id: fad967c21e3c2594066bde2e2f133007e51793e1 [formerly 4b54a04db637ab63f74da45a05c9c2c482e984ad]
    Former-commit-id: 1e848f5cd86d22df5bfdf995dbebe6452b84b562

--- doc-js/io_simple.js
+++ doc-js/io_simple.js
@@ -6,8 +6,9 @@ require(["jquery","cs!seq", "cs!input/fasta"], function ($, Seq, Fasta) {
   $("#ele").append("Translate       " + a_sequence.translate()+ "\n");
   $("#ele").append("Ungap           " + a_sequence.ungap()+ "\n");
 
-  // uniprot does not support SSL requests
-  var dummyFasta = new Fasta("dummy/S5G603.fasta");
-  $("#ele").append("\n Dummy FASTA: \n" + dummyFasta.toString()+ "\n");
+  // as a async, non-blocking call
+  Fasta.read("dummy/S5G603.fasta", function(fasta) {
+    $("#ele").append("\n Dummy FASTA: \n" + fasta.toString()+ "\n");
+  });
 
 });
