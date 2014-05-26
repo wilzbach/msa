431aed5662eb802646b95bb59b9f4c7e5c3d1c3b
Sebastian Wilzbach <sebi@wilzbach.me>
2014-05-26 07:00:07 +0200

    awesome testing with qunit and cs
    Former-commit-id: d64f4d842c6df4374be1f8682b4c0c87e33a59cd [formerly c71ac4ab46fdc56414b88a03feb8755cd4f1bafb]
    Former-commit-id: e77deded8ae457d7f5339741a75475b10dbe5592

---
+++ tests/test_clustal.js
@@ -0,0 +1,26 @@
+define(["cs!input/clustal"], function (Clustal) {
+  return function(){
+
+    module("clustal");
+
+    // as a async, non-blocking call
+    asyncTest( "simple clustal test", function() {
+      expect(1);
+
+      Clustal.read("../dummy/samples/p53.clustalo.clustal", function(seqs) {
+        equal(34, seqs.length, "invalid seq length");
+        start();
+      });
+    });
+
+    // as a async, non-blocking call
+    asyncTest( "simple clustal test2", function() {
+      expect(1);
+
+      Clustal.read("../dummy/samples/p53.clustalo.clustal", function(seqs) {
+        equal(34, seqs.length, "invalid seq length");
+        start();
+      });
+    });
+  };
+});
