b299952baf35e8b40fe56037e0e57d13b55857ac
Sebastian Wilzbach <sebi@wilzbach.me>
2014-05-26 06:59:48 +0200

    refactoring highlights + events to coffeescript
    Former-commit-id: 3625da254ea49a883df9bc57a662ddbac8c5b69c [formerly 091db9738f33a16c765262c3d1dfb9ffd062e3dc]
    Former-commit-id: 39925bce8a576b550563d6626df18dbfb13b7af8

--- js/msa/msa.js
+++ js/msa/msa.js
@@ -1,4 +1,4 @@
-define(["./colorator", "./sequence", "./ordering", "./menu", "./utils", "./labelcolorator", "./row"], function(Colorator, Sequence, Ordering, Menu, Utils, LabelColorator, Row) {
+define(["./colorator", "./sequence", "./ordering", "./menu", "./utils", "./labelcolorator", "./row", "cs!./highlightor", "cs!msa/eventhandler"], function(Colorator, Sequence, Ordering, Menu, Utils, LabelColorator, Row, Highlightor, Eventhandler) {
   return function msa(divName, config){
 
     var _self = this;
@@ -16,16 +16,14 @@ define(["./colorator", "./sequence", "./ordering", "./menu", "./utils", "./label
     this.colorscheme = new Colorator(); 
     this.labelColorScheme = new LabelColorator();
     this.ordering = new Ordering();
+    this.highlightor = new Highlightor(this);
+
     this.visibleElements = {
       labels: true, sequences: true, menubar: true, ruler: true};
 
 
     // an Array of BioJS.MSA.Row
     this.seqs = [];
-
-    this._currentRow= -1;
-    this._currentColumn= -1;
-    this._currentPos = {'x': -1, 'y': -1 };
 
 
     // create menubar  + canvas
@@ -122,26 +120,23 @@ define(["./colorator", "./sequence", "./ordering", "./menu", "./utils", "./label
         residueSpan.addEventListener('click',function(evt){
 
           var id = this.parentNode.seqid;
-          _self.selectResidue(id,this.rowPos);
+          _self.highlightor.selectResidue(id,this.rowPos);
           // send event
-          _self.onPositionClicked(id,this.rowPos);
+          _self.events.onPositionClicked(id,this.rowPos);
         }, false);
 
 
         if( this.registerMoveOvers === true){
           residueSpan.addEventListener('mouseover',function(evt){
-
             var id = this.parentNode.seqid;
-            _self.selectResidue(id,this.rowPos);
-            // send event
-            _self.onPositionClicked(id,this.rowPos);
+            _self.highlightor.selectResidue(id,this.rowPos);
+            _self.events.onPositionClicked(id,this.rowPos);
           }, false);
         }
 
 
         // color it nicely
         this.colorscheme.colorResidue(residueSpan, tSeq, n);
-
         residueGroup.appendChild(residueSpan);
       }
 
@@ -168,23 +163,14 @@ define(["./colorator", "./sequence", "./ordering", "./menu", "./utils", "./label
         residueSpan.rowPos = n;
 
         residueSpan.addEventListener('click',function(evt){
-
-          _self._cleanupSelections();
-
-          // color the selected residues
-          _self.selectColumn(this.rowPos);
-
-          // send events
-          _self.onColumnSelect(this.rowPos);
-
+          _self.highlightor.selectColumn(this.rowPos);
+          _self.events.onColumnSelect(this.rowPos);
         }, false);
 
         if( this.registerMoveOvers === true){
           residueSpan.addEventListener('mouseover',function(evt){
-            _self._cleanupSelections();
-            _self.selectColumn(this.rowPos);
-            // send event to others
-            _self.onColumnSelect(this.rowPos);
+            _self.highlightor.selectColumn(this.rowPos);
+            _self.events.onColumnSelect(this.rowPos);
           }, false);
         }
 
@@ -210,20 +196,19 @@ define(["./colorator", "./sequence", "./ordering", "./menu", "./utils", "./label
 
       labelGroup.addEventListener('click',function(evt){
         var id = this.seqid;
-        _self.onRowSelect(id);
-        _self.selectSeq(id);
+        _self.highlightor.selectSeq(id);
+        _self.events.onRowSelect(id);
       }, false);
 
       if( this.registerMoveOvers === true){
         labelGroup.addEventListener('mouseover',function(evt){
           var id = this.seqid;
-          _self.onRowSelect(id);
-          _self.selectSeq(id);
+          _self.highlightor.selectSeq(id);
+          _self.events.onRowSelect(id);
         }, false);
       }
 
       this.labelColorScheme.colorLabel(labelGroup,tSeq);
-
       return labelGroup;
     };
 
@@ -281,123 +266,12 @@ define(["./colorator", "./sequence", "./ordering", "./menu", "./utils", "./label
       this.stage.width = ( this.seqOffset + nMax * this.columnWidth);
 
     };
-
-    /**
-     * Selects a row (does not send any event)
-     */
-    this.selectSeq = function(id){
-
-      this._cleanupSelections();
-
-      var currentLayer = _self.seqs[id].layer;
-      var tSeq = _self.seqs[id].tSeq;
-
-      // color selected row
-      var childs = currentLayer.childNodes[1].childNodes;
-      for(var i=0; i< childs.length; i++){
-        _self.colorscheme.colorSelectedResidue(childs[i],tSeq,i);
-      }
-
-      // label
-      var currentLayerLabel = currentLayer.children[0];
-      _self.labelColorScheme.colorSelectedLabel(currentLayerLabel, tSeq);
-
-      _self._currentRow =id;
-    };
-
-    this.removeSeq  = function(id){
-      this.seqs[id].layer.destroy();
-      delete this.seqs[id];
-
-      //reorder ??
-    };
-
-
-    this.selectResidue = function (id,rowPos){
-      this._cleanupSelections();
-
-      var tSeq = _self.seqs[id].tSeq;
-
-      // color the selected residue
-      var singleResidue = _self.seqs[id].layer.children[1].children[rowPos];
-      _self.colorscheme.colorSelectedResidueSingle(singleResidue,tSeq,rowPos);
-
-      // send events
-      _self.onPositionClicked(id,rowPos);
-      _self._currentPos.y = id;
-      _self._currentPos.x = rowPos;
-    };
-
-    this.selectColumn = function(selColumn){
-      this._cleanupSelections();
-
-      if( selColumn >= 0 ){
-        var columnGroup = _self._seqMarkerLayer.childNodes[selColumn];
-        _self.colorscheme.colorSelectedColumn(columnGroup, selColumn);
-        for( var seq in _self.seqs){
-          var singlePos = _self.seqs[seq].layer.children[1].childNodes[selColumn];
-          _self.colorscheme.colorSelectedResidueColumn(singlePos,_self.seqs[seq].tSeq,singlePos.rowPos);
-        }
-      }
-      _self._currentColumn = selColumn;
-    }
-
-
-    /*
-     * removes all existing selections
-     */
-    this._cleanupSelections = function(){
-
-      this._removeHorizontalSelection();
-      this._removeVerticalSelection();
-      this._removePositionSelection();
-    }
-
-    this._removePositionSelection = function(){
-      var currentPos = _self._currentPos;
-      if( currentPos.x >= 0 && currentPos.y >= 0){
-        var singlePos =_self.seqs[currentPos.y].layer.childNodes[1].childNodes[currentPos.x];
-        var tSeq = _self.seqs[currentPos.y].tSeq;
-        _self.colorscheme.colorResidue(singlePos,tSeq, currentPos.x);
-      }
-    }
-
-    this._removeVerticalSelection = function(){
-      var currentColumn = _self._currentColumn;
-      if( currentColumn >= 0 ){
-        var columnGroup = _self._seqMarkerLayer.childNodes[currentColumn];
-        _self.colorscheme.colorColumn(columnGroup, currentColumn);
-        for( var seq in _self.seqs){
-          var singlePos = _self.seqs[seq].layer.childNodes[1].childNodes[currentColumn];
-          _self.colorscheme.colorResidue(singlePos,_self.seqs[seq].tSeq,singlePos.rowPos);
-        }
-      }
-    }
-
-    this._removeHorizontalSelection = function(){
-      var currentRow = _self._currentRow;
-      if(currentRow >= 0){
-        var tSeq = _self.seqs[currentRow].tSeq;
-        var currentLayer = _self.seqs[currentRow].layer;
-
-        // all residues
-        var childs = currentLayer.childNodes[1].childNodes;
-        for(var i=0; i< childs.length; i++){
-          _self.colorscheme.colorResidue(childs[i],tSeq,i);
-        }
-
-        // label
-        var currentLayerLabel = currentLayer.childNodes[0];
-        _self.labelColorScheme.colorLabel(currentLayerLabel,tSeq);
-      }
-    }
-
     /*
      * redraws the entire MSA
      */
     this.redraw = function(){
 
-      this._cleanupSelections();
+      this.highlightor._cleanupSelections();
 
       // all columns
       for( var curRow in _self.seqs){
@@ -418,11 +292,18 @@ define(["./colorator", "./sequence", "./ordering", "./menu", "./utils", "./label
       }
     };
 
+    this.removeSeq = function(id){
+      seqs[id].layer.destroy()
+      delete seqs[id]
+
+      // reorder
+      this.orderSeqsAfterScheme()
+    };
+
 
     this.redrawEntire = function(){
 
       var tSeqs = [];
-
       for( var tSeq in this.seqs){
         tSeqs.push(this.seqs[tSeq].tSeq);
       }
@@ -435,48 +316,7 @@ define(["./colorator", "./sequence", "./ordering", "./menu", "./utils", "./label
     this.resetStage = function() {
       Utils.removeAllChilds(this.stage);
     }
-    /*
-     * EVENTS start here
-     */
-
-    this.onColumnSelect = function(pos){
-      _self.log("column was clicked at pos"+ pos);
-    }
-
-    this.onRowSelect = function(id){
-      _self.log("row was clicked at id"+ id);
-    }
-
-    this.onPositionClicked = function(id, pos){
-      _self.log("seq " +id +" was clicked at " + pos);
-    }
-
-    this.onAnnotationClicked = function(){
-      _self.log("not implemented yet");
-    }
-
-    this.onRegionSelected = function(){
-      _self.log("not implemented yet");
-    }
-
-    this.onZoom = function(){
-      _self.log("not implemented yet");
-    }
-
-    this.onScroll = function(){
-      _self.log("not implemented yet");
-    }
-
-    this.onColorSchemeChanged = function(){
-      _self.log("not implemented yet");
-    }
-
-    this.onDisplayEventChanged = function(){
-      _self.log("not implemented yet");
-    }
-
-
-    this.console = undefined;
+      this.console = undefined;
 
     this.setConsole = function(name){
       this.console = document.getElementById(name);
@@ -495,6 +335,7 @@ define(["./colorator", "./sequence", "./ordering", "./menu", "./utils", "./label
       //  console.log(msg);
     };
 
-  };
+    this.events = new Eventhandler(this.log);
 
+  };
 });
