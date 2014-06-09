c73f4026cc527e8deeb3c75d33073b88350afa22
Sebastian Wilzbach <sebi@wilzbach.me>
2014-06-09 06:48:47 +0200

    cli parser for build + fix for .main files

--- doc-js/msa_highlighting.js
+++ doc-js/msa_highlighting.js
@@ -1,7 +1,11 @@
-require(["msa/msa"], function (MSA) {
+require(["msa/msa", "msa/selection/main"], function (MSA, selection) {
 
   var msa = new MSA('msa-highlighting');
   msa.setConsole('msa-highlighting-console');
-
   msa.addDummySequences();
+
+  var selList = new selection.SelectionList();
+  selList.addSelection(new selection.VerticalSelection(msa,2));
+  selList.addSelection(new selection.HorizontalSelection(msa,3));
+
 });
