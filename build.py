ec337077ac51438a437b90fba21e6183d6ff3cb2
Sebastian Wilzbach <sebi@wilzbach.me>
2014-06-09 11:02:12 +0200

    build error for documentation css fixed

--- build.py
+++ build.py
@@ -100,7 +100,7 @@ def main():
     print("Copying static files and libs")
     distutils.dir_util.copy_tree(path.join(rootDir, 'dummy'), path.join(buildDir, 'dummy'))
     distutils.dir_util.copy_tree(path.join(rootDir, 'libs'), path.join(buildDir, 'libs'))
-    distutils.dir_util.copy_tree(path.join(rootDir, 'doc-css'), path.join(buildDir, 'css'))
+    distutils.dir_util.copy_tree(path.join(rootDir, 'doc-css'), path.join(buildDir, 'doc-css'))
     distutils.dir_util.copy_tree(path.join(rootDir, 'doc-js'), path.join(buildDir, 'doc-js'))
     print("\nEverything is ok. You rock!")
 
