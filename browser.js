99807e2e5f9bac80239d0ed20f7a681d235053af
Sebastian Wilzbach <sebi@wilzbach.me>
2014-08-17 21:46:45 +0200

    fix for global namespace and zoomer config

--- browser.js
+++ browser.js
@@ -5,7 +5,7 @@ if (typeof biojs.vis === 'undefined') {
   module.exports = biojs.vis = {};
 }
 // use two namespaces
-msa = biojs.vis.msa = require('./index');
+window.msa = biojs.vis.msa = require('./index');
 
 // TODO: how should this be bundled
 biojs.vis.easy_features = require("biojs-vis-easy_features");
