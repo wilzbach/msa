b4f14ddd8e92905619ef998b5fd563286f2c4c23
Sebastian Wilzbach <sebi@wilzbach.me>
2014-09-10 22:11:55 +0200

    added medium and large dataset snippet for #14

---
+++ snippets/medium_set.js
@@ -0,0 +1,16 @@
+var opts = {};
+opts.seqs = msa.utils.seqgen.getDummySequences(300,300);
+opts.el = yourDiv;
+opts.vis = {metacell: true, overviewbox: true};
+var m = new msa.msa(opts);
+
+// the menu is independent to the MSA container
+var menuOpts = {};
+var menuDiv = document.createElement('div');
+document.body.appendChild(menuDiv);
+menuOpts.el = menuDiv;
+menuOpts.msa = m;
+var defMenu = new msa.menu.defaultmenu(menuOpts);
+
+m.addView("menu", defMenu);
+m.render();
