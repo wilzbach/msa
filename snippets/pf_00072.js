e9d2a3944a72b37cd0b53fb70b89b73a6715a797
Sebastian Wilzbach <sebi@wilzbach.me>
2014-09-12 15:54:35 +0200

    PFAM 15k data set
    Former-commit-id: 509eb44b8c788df82d2ce976a20c166e582b26cd [formerly b945e48340b203c967351ae964f0cacbf0289f15]
    Former-commit-id: 6864c5d89dd854a4266061602f5fc5f712f3b8b9

---
+++ snippets/pf_00072.js
@@ -0,0 +1,24 @@
+yourDiv.textContent = "loading. please wait."
+
+biojs.io.fasta.parse.read('http://dev.biojs-msa.org/v1/dummy/external/PF00072_rp15.txt', function(seqs) {
+var opts = {};
+opts.seqs = seqs;
+opts.el = yourDiv;
+opts.vis = {conserv: false, overviewbox: true}
+opts.zoomer = {boxRectHeight: 1, boxRectWidth: 1}
+var m = new msa.msa(opts);
+
+// the menu is independent to the MSA container
+var menuOpts = {};
+var menuDiv = document.createElement('div');
+document.body.appendChild(menuDiv);
+menuOpts.el = menuDiv;
+menuOpts.msa = m;
+var defMenu = new msa.menu.defaultmenu(menuOpts);
+//defMenu.createMenu();
+
+m.addView("menu", defMenu);
+
+m.render();
+
+});
