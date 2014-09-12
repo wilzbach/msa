927935c8acdc26dac9256c2f2b1e58789b35c8ee
Sebastian Wilzbach <sebi@wilzbach.me>
2014-09-12 02:22:38 +0200

    working vertical scrolling for canvas-speed
    Former-commit-id: 49c3355b2c1aee075a6eaddb629ddeff0053b996 [formerly bf7eaecafed9678048bbf0776086d4c1b4553f6c]
    Former-commit-id: 591e57f0ba5ddb81b0ef216d1509c57aeca40c9a

--- snippets/large_set.js
+++ snippets/large_set.js
@@ -2,6 +2,7 @@ var opts = {};
 opts.seqs = msa.utils.seqgen.getDummySequences(1000,300);
 opts.el = yourDiv;
 opts.vis = {metacell: true, overviewbox: true};
+opts.zoomer = {boxRectHeight: 1, boxRectWidth: 1}
 var m = new msa.msa(opts);
 
 // the menu is independent to the MSA container
