2182d52d905b36bbad9a797b051a1d9b40c08de5
Sebastian Wilzbach <sebi@wilzbach.me>
2014-09-16 01:23:58 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+jQuery(function($) {
+
+	$(function(){
+		$('#main-slider.carousel').carousel({
+			interval: 7000,
+			pause: false
+		});
+	});
+
+
+	//smooth scroll
+	$('.navbar-nav > li').click(function(event) {
+		event.preventDefault();
+		var target = $(this).find('>a').prop('hash');
+		$('html, body').animate({
+			scrollTop: $(target).offset().top
+		}, 500);
+	});
+
+	//scrollspy
+	$('[data-spy="scroll"]').each(function () {
+		var $spy = $(this).scrollspy('refresh')
+	})
+
+});
