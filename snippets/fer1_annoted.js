dd0a7b9a003a762577ea143d743cbfeb6f287b5f
Sebastian Wilzbach <sebi@wilzbach.me>
2014-12-28 08:49:37 +0100

    removed redundant zoomer properties

--- snippets/fer1_annoted.js
+++ snippets/fer1_annoted.js
@@ -30,13 +30,7 @@ opts.vis = {
 opts.zoomer = {
   alignmentHeight: 225,
   labelWidth: 130,
-  labelFontsize: "13px",
   labelIdLength: 20,
-  menuFontsize: "14px",
-  menuMarginLeft: "3px",
-  menuPadding: "3px 4px 3px 4px",
-  menuItemFontsize: "14px",
-  menuItemLineHeight: "14px"
 };
 
 // init msa
--- snippets/pf_00072.js
+++ snippets/pf_00072.js
@@ -3,24 +3,34 @@ yourDiv.textContent = "loading. please wait."
 var fasta = require("biojs-io-fasta");
 
 fasta.read('http://dev.biojs-msa.org/v1/dummy/external/PF00072_rp15.txt', function(seqs) {
-var opts = {};
-opts.seqs = seqs;
-opts.el = yourDiv;
-opts.vis = {conserv: false, overviewbox: false}
-opts.zoomer = {boxRectHeight: 1, boxRectWidth: 1, labelWidth: 270,labelFontsize: "12px",labelIdLength: 50, alignmentHeight: window.innerHeight * 0.85}
-var m = msa(opts);
+  var opts = {};
+  opts.seqs = seqs;
+  opts.el = yourDiv;
+  opts.vis = {
+    conserv: false,
+    overviewbox: false
+  }
+  opts.zoomer = {
+    boxRectHeight: 1,
+    boxRectWidth: 1,
+    labelWidth: 270,
+    labelFontsize: 12,
+    labelIdLength: 50,
+    alignmentHeight: window.innerHeight * 0.85
+  }
+  var m = msa(opts);
 
-// the menu is independent to the MSA container
-var menuOpts = {};
-var menuDiv = document.createElement('div');
-document.body.appendChild(menuDiv);
-menuOpts.el = menuDiv;
-menuOpts.msa = m;
-var defMenu = new msa.menu.defaultmenu(menuOpts);
-//defMenu.createMenu();
+  // the menu is independent to the MSA container
+  var menuOpts = {};
+  var menuDiv = document.createElement('div');
+  document.body.appendChild(menuDiv);
+  menuOpts.el = menuDiv;
+  menuOpts.msa = m;
+  var defMenu = new msa.menu.defaultmenu(menuOpts);
+  //defMenu.createMenu();
 
-m.addView("menu", defMenu);
+  m.addView("menu", defMenu);
 
-m.render();
+  m.render();
 
 });
