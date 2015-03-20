26951d542a3a6cc7a9a75cfefb6cbfa0cc544142
Sebastian Wilzbach <seb@wilzba.ch>
2015-03-20 06:46:34 +0100

    simplified msa snippet
    Former-commit-id: 66da6f034ac7e6d80c67251e1456d5b65ae7c5da [formerly c444b4f9bd6659718c6d766d9d300113af7c40a9]
    Former-commit-id: 5ccc57fe9f9424682bdb6540e4b1335cd0aef6d5

---
+++ snippets/simple.js
@@ -0,0 +1,9 @@
+/* global rootDiv */
+var msa = require("msa");
+var opts = {
+  el: rootDiv,
+  importURL: "./data/fer1.clustal",
+  bootstrapMenu: true, // simplified behavior to add the menu bar, you can also create your own menu instance
+};
+var m = new msa(opts);
+//@biojs-instance=m.g
