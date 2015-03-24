854f156646fdb9b45dead463c9ea44c51b0c61bc
Sebastian Wilzbach <seb@wilzba.ch>
2015-03-24 12:43:44 -0400

    updated snippets
    Former-commit-id: c48bfc1adde43355a4db63b7840a783a9bf48424 [formerly 04ede032db2db015db174bce51372f721ffd313b]
    Former-commit-id: 1b3a4e79f903442a6000a3b71d6edc861812530d

--- snippets/pf_00072.js
+++ snippets/pf_00072.js
@@ -2,7 +2,7 @@ var msa = require("msa");
 yourDiv.textContent = "loading. please wait."
 var fasta = require("biojs-io-fasta");
 
-fasta.read('http://dev.biojs-msa.org/v1/dummy/external/PF00072_rp15.txt', function(seqs) {
+fasta.read('https://cdn.rawgit.com/greenify/msa-example-files/master/PF00072_rp15.txt', function(seqs) {
   var opts = {};
   opts.seqs = seqs;
   opts.el = yourDiv;
@@ -18,6 +18,7 @@ fasta.read('http://dev.biojs-msa.org/v1/dummy/external/PF00072_rp15.txt', functi
     labelIdLength: 50,
     alignmentHeight: window.innerHeight * 0.85
   }
+  console.log(seqs.length);
   var m = msa(opts);
 
   // the menu is independent to the MSA container
