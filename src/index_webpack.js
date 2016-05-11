0f51f9fe6121034adca0f1c8a5cbe1c37138cdcd
Sebastian Wilzbach <seb@wilzba.ch>
2016-05-11 12:14:54 +0300

    fix es6 exports for the commonjs default constructor
    Former-commit-id: 70137d9d1de44400f4ce409d9b0816cf51ba04e8 [formerly 06cd583cfe82b8e70ab789f04df308ae1fe069f0]
    Former-commit-id: 7d9826b43d36350173a598700bb3a9a2776d2c7a

--- src/index_webpack.js
+++ src/index_webpack.js
@@ -1,7 +1,15 @@
 // trick to bundle the css
 require('./../css/msa.css');
-import * as MSA from "./index";
-if (!!window) {
-    window.msa = MSA;
+import * as MSA from './index';
+const msa = MSA.default;
+// workaround against es6 exports
+// we want to expose the MSA constructor by default
+for (var key in MSA) {
+    if (MSA.hasOwnProperty(key)) {
+        msa[key] = MSA[key];
+    }
 }
-export default MSA;
+if (!!window) {
+    window.msa = msa;
+}
+module.exports = msa;
