7bed3ca18fca71d974dab63beea85861cc4f2c2f
Sebastian Wilzbach <seb@wilzba.ch>
2016-05-12 12:32:54 +0300

    better s3 setup
    Former-commit-id: 9d93f79da66af37926713328de858afbe90a8fc6 [formerly 4658b0743f84faa8d324058317eee28e34b8c852]
    Former-commit-id: 687bae6d8d45d198840b28a66b03abdc3ce7f4dc

--- cdn.sh
+++ cdn.sh
@@ -2,9 +2,25 @@
 
 npm run build
 
-echo "uploading to s3"
+# current version
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
 
-s3cmd --acl-public -m "application/javascript" put dist/msa.js s3://cdn.bio.sh/msa/latest/msa.js
-s3cmd --acl-public -m "application/octet-stream" put dist/msa.js.map s3://cdn.bio.sh/msa/latest/msa.js.map
-s3cmd --acl-public -m "application/javascript" --add-header='Content-Encoding: gzip' put dist/msa.min.gz.js s3://cdn.bio.sh/msa/latest/msa.min.gz.js
-s3cmd --acl-public -m "application/octet-stream" put dist/msa.js.map s3://cdn.bio.sh/msa/latest/msa.min.js.map
+MINOR_VERSION=$(echo $PACKAGE_VERSION | awk -F. '{printf "%s.%s", $1, $2}')
+MAJOR_VERSION=$(echo $PACKAGE_VERSION | awk -F. '{print $1}')
+
+echo "LATEST: ${PACKAGE_VERSION}"
+
+for folder in "latest" ${PACKAGE_VERSION} ${MINOR_VERSION} ${MAJOR_VERSION} ; do
+
+echo "uploading to s3 folder: $folder"
+
+s3cmd --acl-public -m "application/javascript" put dist/msa.js "s3://cdn.bio.sh/msa/${folder}/msa.js"
+s3cmd --acl-public -m "application/octet-stream" put dist/msa.js.map "s3://cdn.bio.sh/msa/${folder}/msa.js.map"
+s3cmd --acl-public -m "application/javascript" --add-header='Content-Encoding: gzip' put dist/msa.min.gz.js "s3://cdn.bio.sh/msa/${folder}/msa.min.gz.js"
+s3cmd --acl-public -m "application/octet-stream" put dist/msa.js.map "s3://cdn.bio.sh/msa/${folder}/msa.min.js.map"
+
+done
