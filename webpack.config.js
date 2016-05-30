6fcc556226b2bc2378b35cf58ffc3ff3cdf790c9
Sebastian Wilzbach <seb@wilzba.ch>
2016-05-30 21:07:23 +0200

    strip console.log on production builds
    Former-commit-id: c5e453f5abb4e11579386135fa26101a01f2e811 [formerly 24057c1afb76a476a1b698bdcdc37f1199838ef7]
    Former-commit-id: a87215750727b6c716fc932713b8001c6ffa4447

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
+var prod = process.argv.indexOf('-p') >= 0;
+
 module.exports = {
     entry: './src/index_webpack.js',
     output: {
@@ -19,6 +21,23 @@ module.exports = {
     plugins: [
         new webpack.DefinePlugin({
             MSA_VERSION: JSON.stringify(require("./package.json").version)
-        })
-    ]
+        }),
+    ],
 };
+var w = module.exports;
+
+// only executed with -p
+if(prod) {
+    w.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                collapse_vars: true
+            },
+        sourceMap: true
+    }));
+    var WebpackStrip = require('strip-loader');
+    w.module.loaders.push(
+        {   test: path.join(__dirname, 'src'),
+         loader: WebpackStrip.loader('console.log') }
+    );
+}
