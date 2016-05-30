27d1a9a37061c1e44d4483b9643b1caed05abed8
Sebastian Wilzbach <seb@wilzba.ch>
2016-05-30 18:03:01 +0200

    Fix remaining non ES-6 code
    Former-commit-id: 1bf85509a6fd89e182cb7bc156cd95a365eb6c46 [formerly e047b8cf301622cfcdea6cb971fb8118c4d7b842]
    Former-commit-id: 6173cd7260bc798cb5b742d3b04920001eab8c5b

--- src/views/canvas/CanvasSelection.js
+++ src/views/canvas/CanvasSelection.js
@@ -13,22 +13,22 @@ _.extend(CanvasSelection.prototype, {
   // returns an array with the currently selected residues
   // e.g. [0,3] = pos 0 and 3 are selected
   _getSelection: function(model) {
-    var maxLen = model.get("seq").length;
-    var selection = [];
-    var sels = this.g.selcol.getSelForRow(model.get("id"));
-    var rows = _.find(sels, function(el) { return el.get("type") === "row"; });
+    const maxLen = model.get("seq").length;
+    const selection = [];
+    const sels = this.g.selcol.getSelForRow(model.get("id"));
+    const rows = _.find(sels, function(el) { return el.get("type") === "row"; });
     if ((typeof rows !== "undefined" && rows !== null)) {
       // full match
-      var end = maxLen - 1;
-      for (var n = 0; n <= end; n++) {
+      const end = maxLen - 1;
+      for (let n = 0; n <= end; n++) {
         selection.push(n);
       }
     } else if (sels.length > 0) {
-      for (var i = 0, sel; i < sels.length; i++) {
+      for (let i = 0, sel; i < sels.length; i++) {
         sel = sels[i];
-        var start = sel.get("xStart");
-        var end = sel.get("xEnd");
-        for (var n = start; n <= end; n++) {
+        const start = sel.get("xStart");
+        const end = sel.get("xEnd");
+        for (let n = start; n <= end; n++) {
           selection.push(n);
         }
       }
@@ -39,22 +39,24 @@ _.extend(CanvasSelection.prototype, {
 
   // loops over all selection and calls the render method
   _appendSelection: function(data) {
-    var seq = data.model.get("seq");
-    var selection = this._getSelection(data.model);
+    const seq = data.model.get("seq");
+    const selection = this._getSelection(data.model);
     // get the status of the upper and lower row
-    var [mPrevSel,mNextSel] = this._getPrevNextSelection(data.model);
+    const getNextPrev= this._getPrevNextSelection(data.model);
+    const mPrevSel = getNextPrev[0];
+    const mNextSel = getNextPrev[1];
 
-    var boxWidth = this.g.zoomer.get("columnWidth");
-    var boxHeight = this.g.zoomer.get("rowHeight");
+    const boxWidth = this.g.zoomer.get("columnWidth");
+    const boxHeight = this.g.zoomer.get("rowHeight");
 
     // avoid unnecessary loops
     if (selection.length === 0) { return; }
 
-    var hiddenOffset = 0;
+    let hiddenOffset = 0;
     return (() => {
-      var result = [];
-      var end = seq.length - 1;
-      for (var n = 0; 0 < end ? n <= end : n >= end; 0 < end ? n++ : n--) {
+      const result = [];
+      const end = seq.length - 1;
+      for (let n = 0; 0 < end ? n <= end : n >= end; 0 < end ? n++ : n--) {
         result.push((() => {
           if (data.hidden.indexOf(n) >= 0) {
             return hiddenOffset++;
@@ -62,7 +64,14 @@ _.extend(CanvasSelection.prototype, {
             var k = n - hiddenOffset;
             // only if its a new selection
             if (selection.indexOf(n) >= 0 && (k === 0 || selection.indexOf(n - 1) < 0 )) {
-              return this._renderSelection({n:n,k:k,selection: selection,mPrevSel: mPrevSel,mNextSel:mNextSel, xZero: data.xZero, yZero: data.yZero, model: data.model});
+              return this._renderSelection({n:n,
+                                           k:k,
+                                           selection: selection,
+                                           mPrevSel: mPrevSel,
+                                           mNextSel:mNextSel,
+                                           xZero: data.xZero,
+                                           yZero: data.yZero,
+                                           model: data.model});
             }
           }
         })());
@@ -74,19 +83,19 @@ _.extend(CanvasSelection.prototype, {
   // draws a single user selection
   _renderSelection: function(data) {
 
-    var xZero = data.xZero;
-    var yZero = data.yZero;
-    var n = data.n;
-    var k = data.k;
-    var selection = data.selection;
+    let xZero = data.xZero;
+    const yZero = data.yZero;
+    const n = data.n;
+    const k = data.k;
+    const selection = data.selection;
     // and checks the prev and next row for selection  -> no borders in a selection
-    var mPrevSel= data.mPrevSel;
-    var mNextSel = data.mNextSel;
+    const mPrevSel= data.mPrevSel;
+    const mNextSel = data.mNextSel;
 
     // get the length of this selection
-    var selectionLength = 0;
-    var end = data.model.get("seq").length - 1;
-    for (var i = n; n < end ? i <= end : i >= end; n < end ? i++ : i--) {
+    let selectionLength = 0;
+    const end = data.model.get("seq").length - 1;
+    for (let i = n; n < end ? i <= end : i >= end; n < end ? i++ : i--) {
       if (selection.indexOf(i) >= 0) {
         selectionLength++;
       } else {
@@ -95,25 +104,25 @@ _.extend(CanvasSelection.prototype, {
     }
 
     // TODO: ugly!
-    var boxWidth = this.g.zoomer.get("columnWidth");
-    var boxHeight = this.g.zoomer.get("rowHeight");
-    var totalWidth = (boxWidth * selectionLength) + 1;
+    const boxWidth = this.g.zoomer.get("columnWidth");
+    const boxHeight = this.g.zoomer.get("rowHeight");
+    const totalWidth = (boxWidth * selectionLength) + 1;
 
-    var hidden = this.g.columns.get('hidden');
+    const hidden = this.g.columns.get('hidden');
 
     this.ctx.beginPath();
-    var beforeWidth = this.ctx.lineWidth;
+    const beforeWidth = this.ctx.lineWidth;
     this.ctx.lineWidth = 3;
-    var beforeStyle = this.ctx.strokeStyle;
+    const beforeStyle = this.ctx.strokeStyle;
     this.ctx.strokeStyle = "#FF0000";
 
     xZero += k * boxWidth;
 
     // split up the selection into single cells
-    var xPart = 0;
-    var end1 = selectionLength - 1;
+    let xPart = 0;
+    const end1 = selectionLength - 1;
     for (var i = 0; 0 < end1 ? i <= end1 : i >= end1; 0 < end1 ? i++ : i--) {
-      var xPos = n + i;
+      let xPos = n + i;
       if (hidden.indexOf(xPos) >= 0) {
         continue;
       }
@@ -148,10 +157,11 @@ _.extend(CanvasSelection.prototype, {
   // TODO: this is very naive, as there might be gaps above or below
   _getPrevNextSelection: function(model) {
 
-    var modelPrev = model.collection.prev(model);
-    var modelNext = model.collection.next(model);
-    if ((typeof modelPrev !== "undefined" && modelPrev !== null)) { var mPrevSel = this._getSelection(modelPrev); }
-    if ((typeof modelNext !== "undefined" && modelNext !== null)) { var mNextSel = this._getSelection(modelNext); }
+    const modelPrev = model.collection.prev(model);
+    const modelNext = model.collection.next(model);
+    let mPrevSel, mNextSel;
+    if ((typeof modelPrev !== "undefined" && modelPrev !== null)) { mPrevSel = this._getSelection(modelPrev); }
+    if ((typeof modelNext !== "undefined" && modelNext !== null)) { mNextSel = this._getSelection(modelNext); }
     return [mPrevSel,mNextSel];
   }
 });
