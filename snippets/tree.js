wilzbach/msa

commit 2e3295a2b7eccad8cba6072212109142f11fcc4a

Fixed loading and display of phylogenetic trees in newick format.
Note that for sucessful loading of a newick file, the labels need to agree with the IDs of the MSA sequences (not their name).

Changes include
*) minor bug fixes/changes to the msa-tnt and biojs-io-newick module loader
*) updates of tree dependencies to newest version
*) adapation of tree.js according to work with new dependency versions
*) adapations to example files for the tree snippet


Former-commit-id: bea0e84c394b647d08ef02826d0ede4611eb1785 [formerly d23fb7bd0cea82275bf34fddd15a25637c7b576b]
Former-commit-id: ce2c146665013e98b8f1b88e3c6b5e6428b96744

src/utils/tree.js
@@ -13,29 +13,33 @@ var tf =
     },
 
     showTree: function(newickStr) {
-      var newick = this.require("biojs-io-newick");
+      var newick = require("biojs-io-newick");
       if (typeof newickStr === "string") {
         var newickObj = newick.parse_newick(newickStr);
       } else {
         newickObj = newickStr;
       }
 
-      var mt = this.require("msa-tnt");
+      var mt = require("msa-tnt");
 
       var sel = new mt.selections();
-      var treeDiv = document.createElement("div");
-    //   @msa.el.insertBefore treeDiv, @msa.el.childNodes[0]
-      this.msa.el.appendChild(treeDiv);
+      var treeDiv;
+      if(this.msa.el.childNodes.length === 1){
+        treeDiv = document.createElement("div");
+        this.msa.el.appendChild(treeDiv);
+      } else {
+        console.log('A tree already exists. It will be overridden.');
+        treeDiv = this.msa.el.childNodes[1];
+        treeDiv.innerHTML = '';
+      }
 
-      console.log(this.msa.seqs.models);
-      console.log(newickObj);
-
+      console.log(this.msa.seqs.toJSON());
       var nodes = mt.app({
         seqs: this.msa.seqs.toJSON(),
         tree: newickObj
       });
 
-      console.log("nodes", nodes);
+      console.log("nodes", nodes.seqs);
 
       var t = new mt.adapters.tree({
         model: nodes,
@@ -56,8 +60,7 @@ var tf =
       _.each(nodes.models, function(e) {
         delete e.collection;
         return Object.setPrototypeOf(e, require("backbone-thin").Model.prototype);
-      }
-      );
+      });
 
       this.msa.seqs.reset(nodes.models);
       //@msa.draw()

snippets/data/tree/B2014122194A560KL7I.4.ids.fa
@@ -1,24 +1,24 @@
->Zaire_Mayinga-76
+>id|1|Zaire_Mayinga-76
 MGVTGILQLPRDRFKRTSFFLWVIILFQRTFSIPLGVIHNSTLQVSDVDKLVCRDKLSSTNQLRSVGLNLEGNGVATDVPSATKRWGFRSGVPPKVVNYEAGEWAENCYNLEIKKPDGSECLPAAPDGIRGFPRCRYVHKVSGTGPCAGDFAFHKEGAFFLYDRLASTVIYRGTTFAEGVVAFLILPQAKKDFFSSHPLREPVNATEDPSSGYYSTTIRYQATGFGTNETEYLFEVDNLTYVQLESRFTPQFLLQLNETIYTSGKRSNTTGKLIWKVNPEIDTTIGEWAFWETKKKPH
->Zaire_Kikwit-95
+>id|2|Zaire_Kikwit-95
 MGVTGILQLPRDRFKRTSFFLWVIILFQRTFSIPLGVIHNSTLQVSDVDKLVCRDKLSSTNQLRSVGLNLEGNGVATDVPSATKRWGFRSGVPPKVVNYEAGEWAENCYNLEIKKPDGSECLPAAPDGIRGFPRCRYVHKVSGTGPCAGDFAFHKEGAFFLYDRLASTVIYRGTTFAEGVVAFLILPQAKKDFFSSHPLREPVNATEDPSSGYYSTTIRYQATGFGTNETEYLFEVDNLTYVQLESRFTPQFLLQLNETIYTSGKRSNTTGKLIWKVNPEIDTTIGEWAFWETKKKPH
->Zaire_Eckron-76
+>id|3|Zaire_Eckron-76
 MGVTGILQLPRDRFKRTSFFLWVIILFQRTFSIPLGVIHNSTLQVNDVDKLVCRDKLSSTNQLRSVGLNLEGNGVATDVPSATKRWGFRSGVPPKVVNYEAGEWAENCYNLEIKKPDGSECLPAAPDGIRGFPRCRYVHKVSGTGPCAGDFAFHKEGAFFLYDRLASTVIYRGTTFAEGVVAFLILPQAKKDFFSSHPLREPVNATEDPSSGYYSTTIRYQATGFGTNETEYLFEVDNLTYVQLESRFTPQFLLQLNETIYTSGKRSNTTGKLIWKVNPEIDTTIGEWAFWETKKTSLEKFAVKSCLSQLYQTEPKTSVVRVRRELLPTQGPTQQLKTTKSWLQKIPLQWFKCTVKEGKLQCRI
->Zaire_Gabon-94
+>id|4|Zaire_Gabon-94
 MGVTGILQLPRDRFKRTSFFLWVIILFQRTFSIPLGVIHNSTLQVSDVDKLVCRDKLSSTNQLRSVGLNLEGNGVATDVPSATKRWGFRSGVPPKVVNYEAGEWAENCYNLEIKKPDGSECLPAAPDGIRGFPRCRYVHKVSGTGPCAGDFAFHKEGAFFLYDRLASTVIYRGTTFAEGVVAFLILPQAKKDFFSSHPLREPVNATEDPSSGYYSTTIRYQATGFGTNETEYLFEVDNLTYVQLESRFTPQFLLQLNETRYTSGKRSNTTGKLIWKVNPEIDTTIGEWAFWETKKTSLEKFAVKSCLSQLYQTEPKTSVVRVRRELLPTQGPTQQLKTTKSWLQKIPLQWFKCTVKEGKLQCRI
->Tai_Forest_Cote_d'Ivoire-94
+>id|5|Tai_Forest_Cote_d'Ivoire-94
 MGASGILQLPRERFRKTSFFVWVIILFHKVFSIPLGVVHNNTLQVSDIDKFVCRDKLSSTSQLKSVGLNLEGNGVATDVPTATKRWGFRAGVPPKVVNYEAGEWAENCYNLAIKKVDGSECLPEAPEGVRDFPRCRYVHKVSGTGPCPGGLAFHKEGAFFLYDRLASTIIYRGTTFAEGVIAFLILPKARKDFFQSPPLHEPANMTTDPSSYYHTTTINYVVDNFGTNTTEFLFQVDHLTYVQLEARFTPQFLVLLNETIYSDNRRSNTTGKLIWKINPTVDTSMGEWAFWENKKTSQKPFQVKSCLSYLYQKPRTRSLTRQRRSLLPSPPTTTQAKTTKNWFQRIPLQWFRCKTSRERTQCQPQ
->Bundibugyo
+>id|6|Bundibugyo
 MVTSGILQLPRERFRKTSFFVWVIILFHKVFPIPLGVVHNNTLQVSDIDKLVCRDKLSSTSQLKSVGLNLEGNGVATDVPTATKRWGFRAGVPPKVVNYEAGEWAENCYNLDIKKADGSECLPEAPEGVRGFPRCRYVHKVSGTGPCPEGYAFHKEGAFFLYDRLASTIIYRSTTFSEGVVAFLILPETKKDFFQSPPLHEPANMTTDPSSYYHTVTLNYVADNFGTNMTNFLFQVDHLTYVQLEPRFTPQFLVQLNETIYTNGRRSNTTGTLIWKVNPTVDTGVGEWAFWENKKLHKNPFK
->Reston_Reston-89
+>id|7|Reston_Reston-89
 MGSGYQLLQLPRERFRKTSFLVWVIILFQRAISMPLGIVTNSTLKATEIDQLVCRDKLSSTSQLKSVGLNLEGNGIATDVPSATKRWGFRSGVPPKVVSYEAGEWAENCYNLEIKKSDGSECLPLPPDGVRGFPRCRYVHKVQGTGPCPGDLAFHKNGAFFLYDRLASTVIYRGTTFAEGVVAFLILSEPKKHFWKATPAHEPVNTTDDSTSYYMTLTLSYEMSNFGGNESNTLFKVDNHTYVQLDRPHTPQFLVQLNETLRRNNRLSNSTGRLTWTLDPKIEPDVGEWAFWETKKKLFPTTSWRKLAFPNSINPHQQLLRSEPGGNCPRKN
->Reston_Philippines-96
+>id|8|Reston_Philippines-96
 MGSGYQLLQLPRERFRKTSFLVWVIILFQRAISMPLGIVTNSTLKATEIDQLVCRDKLSSTSQLKSVGLNLEGNGIATDVPSATKRWGFRSGVPPKVVSYEAGEWAENCYNLEIKKSDGSECLPLPPDGVRGFPRCRYVHKVQGTGPCPGDLAFHKNGAFFLYDRLASTVIYRGTTFAEGVIAFLILSEPKKHFWKATPAHEPVNTTDDSTSYYMTLTLSYEMSNFGGEESNTLFKVDNHTYVQLDRPHTPQFLVQLNETLRRNNRLSNSTGRLTWTVDPKIEPDVGEWAFWETKKKLFPTTSWRKLAFPNSINPHQQLLRSEPGGNCPRKN
->Reston_Siena/Philippine-92
+>id|9|Reston_Siena/Philippine-92
 MGSGYQLLQLPRERFRKTSFLVWVIILFQRAISMPLGIVTNSTLKATEIDQLVCRDKLSSTSQLKSVGLNLEGNGIATDVPSATKRWGFRSGVPPKVVSYEAGEWAENCYNLEIKKSDGSECLPLPPDGVRGFPRCRYVHKVQGTGPCPGDLAFHKNGAFFLYDRLASTVIYRGTTFTEGVVAFLILSEPKKHFWKATPAHEPVNTTDDSTSYYMTLTLSYEMSNFGGKESNTLFKVDNHTYVQLDRPHTPQFLVQLNETLRRNNRLSNSTGRLTWTLDPKIEPDVGEWAFWETKKTFPNNFMEKTCISKFYQPTPTTPQIRARRELSKEKLATTHPPTTPSWFQRIPLQWFQCSLQDGQRKCRPKV
->Sudan_Uganda-00
+>id|10|Sudan_Uganda-00
 MGGLSLLQLPRDKFRKSSFFVWVIILFQKAFSMPLGVVTNSTLEVTEIDQLVCKDHLASTDQLKSVGLNLEGSGVSTDIPSATKRWGFRSGVPPKVVSYEAGEWAENCYNLEIKKPDGSECLPPPPDGVRGFPRCRYVHKAQGTGPCPGDYAFHKDGAFFLYDRLASTVIYRGVNFAEGVIAFLILAKPKETFLQSPPIREAVNYTENTSSYYATSYLEYEIENFGAQHSTTLFKIDNNTFVRLDRPHTPQFLFQLNDTIHLHQQLSNTTGRLIWTLDANINADIGEWAFWENKKKSLRTTTWRRAVFRSFIAQRDRRR
->Sudan_Maleo-79
+>id|11|Sudan_Maleo-79
 MEGLSLLQLPRDKFRKSSFFVWVIILFQKAFSMPLGVVTNSTLEVTEIDQLVCKDHLASTDQLKSVGLNLEGSGVSTDIPSATKRWGFRSGVPPQVVSYEAGEWAENCYNLEIKKPDGSECLPPPPDGVRGFPRCRYVHKAQGTGPCPGDYAFHKDGAFFLYDRLASTVIYRGVNFAEGVIAFLILAKPKETFLQSPPIREAANYTENTSSYYATSYLEYEIENFGAQHSTTLFKINNNTFVLLDRPHTPQFLFQLNDTIQLHQQLSNTTGKLIWTLDANINADIGEWAFWENKKISPNNYVEKSCLSKLYRSTRQKTMMRHRRELQREESPTGPPGSIRTWFQRIPLGWFHCTYQKGKQHCRLRIRQKVEE
->Sudan_Boniface-76
+>id|12|Sudan_Boniface-76
 MEGLSLLQLPRDKFRKSSFFVWVIILFQKAFSMPLGVVTNSTLEVTEIDQLVCKDHLASTDQLKSVGLNLEGSGVSTDIPSATKRWGFRSGVPPQVVSYEAGEWAENCYNLEIKKPDGSECLPPPPDGVRGFPRCRYVHKAQGTGPCPGDYAFHKDGAFFLYDRLASTVIYRGVNFAEGVIAFLILAKPKETFLQSPPIREAANYTENTSSYYATSYLEYEIENFGAQHSTTLFKINNNTFVLLDRPHTPQFLFQLNDTIQLHQQLSNTTGKLIWTLDANINADIGEWAFWENKKISPNNYVEKSCLSKLYRSTRQKTMMRHRRELQREESPTGPPGSIRTWFQRIPLGWFHCTYQKGKQHCRLRIRQKVEE

src/g/package.js
@@ -19,13 +19,13 @@ module.exports = Package = Model.extend({
       var p = require(pkg);
       return cb(p);
     } catch (error) {
-      return Loader.loadScript(this._pkgURL(pkg), cb);
+      return Loader.default.loadScript(this._pkgURL(pkg), cb);
     }
   },
 
   // loads multiple packages and calls the cb if all packages are loaded
   loadPackages: function(pkgs, cb) {
-      var cbs = Loader.joinCb( (function() {
+      var cbs = Loader.default.joinCb( (function() {
         return cb();
       }
       ), pkgs.length

commit d5948615f479d696298349cc19efed416b752af2

bug fix fixed I bug I introduced in 2e3295a that would prevent tree upload under certain circumstances (e.g. fer1 example). added dropImport to tree example

snippets/tree.js
@@ -4,6 +4,9 @@ var opts = {
   el: rootDiv,
   importURL: "./data/tree/B2014122194A560KL7I.4.ids.fa",
   bootstrapMenu: true,
+  conf: {
+    dropImport: true
+  }
 };
 var m = new msa(opts);
 m.importURL("./data/tree/B2014122194A560KL7I.4.ids.phylotree.txt", function(){

src/utils/tree.js
@@ -24,12 +24,13 @@ var tf =
 
       var sel = new mt.selections();
       var treeDiv;
-      if(this.msa.el.childNodes.length === 1){
+
+      if(this.msa.el.getElementsByClassName('tnt_groupDiv').length === 0){
         treeDiv = document.createElement("div");
         this.msa.el.appendChild(treeDiv);
       } else {
         console.log('A tree already exists. It will be overridden.');
-        treeDiv = this.msa.el.childNodes[1];
+        treeDiv = this.msa.el.getElementsByClassName('tnt_groupDiv')[0].parentNode;
         treeDiv.innerHTML = '';
       }
 
@@ -41,9 +42,17 @@ var tf =
         } else {
           //found a leave
           let seq = seqs.filter(s => s.name === nwck.name)[0];
+
           if(seq != null){
-            seq.ids = [`s${seq.id + 1}`];
-            nwck.name = `s${seq.id + 1}`;
+            if(typeof seq.id === 'number'){
+              //no tree has been uploaded so far, seqs have standard IDs
+              seq.ids = [`s${seq.id + 1}`];
+              nwck.name = `s${seq.id + 1}`;
+            } else {
+              //seqs have custom ids - don't mess with these
+              console.log('hi');
+              nwck.name = seq.id;
+            }
           }
         }
       }
@@ -53,8 +62,6 @@ var tf =
         seqs: seqs,
         tree: newickObj
       });
-
-      console.log("nodes", nodes.seqs);
 
       var t = new mt.adapters.tree({
         model: nodes,