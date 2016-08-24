b63e37322d5734ea2181884446f6ad0d533e4939
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2016-08-24 16:01:46 +0200

    keep old default values

--- src/g/zoomer.js
+++ src/g/zoomer.js
@@ -53,8 +53,8 @@ module.exports = Zoomer = Model.extend({
     boxRectHeight: 2,
     boxRectWidth: 2,
     overviewboxPaddingTop: 10,
-    overviewboxWidth: "auto",  // "auto" (fitting div) or "fixed"
-    overviewboxHeight: 200,    // "fixed" or in px
+    overviewboxWidth: "fixed",   // "auto" (fitting div) or "fixed"
+    overviewboxHeight: "fixed",  // "fixed" or in px
 
     // meta cell
     metaGapWidth: 35,
